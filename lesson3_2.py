29f1545f5166a2b653ba6c30a44486db46aeea4f
oogway <oogway543543@gmail.com>
2023-06-13 11:57:06 +0800

    0

--- lesson3_2.py
+++ lesson3_2.py
@@ -1,23 +1,27 @@
 import tkinter as tk
 from tkinter import ttk
 
-
 class Window(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title('hello')
         self.resizable(False, False)
         s = ttk.Style()
-        s.configure('Title.TLabl',foreground='red', background='black',font=('Arial',20))
-        s.configure('Title.TButton',foreground='red', background='black',font=('Arial',20))
-        s.theme_use('clam')        
-        title_label = ttk.Label(self,text="LED控制器",style='Title.TLabl')
-        print(title_label.winfo_class())
+        s.theme_use('clam')
+        s.configure('Title.TLabel',foreground='red',background='black',font=('Arial',20))
+        s.configure('Led.TButton',foreground='red',background='yellow',font=('Arial',20), borderwidth=5, padding=(10,20))
+        print(s.layout('TButton'))
+        title_label = ttk.Label(self,text="LED控制器",style='Title.TLabel')        
         title_label.pack(pady=25,padx=100)
 
-        led_btn = ttk.Button(self,text="LED 開")
+        led_btn = ttk.Button(self,text="LED 開",style='Led.TButton',command=self.user_click)   
+             
         led_btn.pack(pady=(10,50))
+
+    def user_click(self):
+        print('user click')
 
 if __name__ == "__main__":
     window = Window()   
     window.mainloop()
+   
