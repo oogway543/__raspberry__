cd538757610c706f54624959423fbb3722f9443c
oogway <oogway543543@gmail.com>
2023-06-15 15:47:21 +0800

    0

--- datasource.py
+++ datasource.py
@@ -13,9 +13,10 @@ def create_connection(db_file):
 def create_table(conn):
     sql_projects = """
     CREATE TABLE IF NOT EXISTS led (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        date text NOT NULL,
-        state INTEGER NOT NULL
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                date TEXT NOT NULL,
+                state INTEGER NOT NULL
+    );
     """
 
     try:
@@ -24,3 +25,12 @@ def create_table(conn):
         print("Table created successfully")
     except Error as e:
         print(e)
+
+def insert_data(conn, state):
+    sql_insert = """
+    INSERT INTO led(date, state)VALUES(datetime('now'), ?);
+    """
+    cursor = conn.cursor()
+    cursor.execute(sql_insert, (state,))
+    conn.commit()
+    conn.close()
