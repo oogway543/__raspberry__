5af729d610ce76ff169bb758020d21c65d66a993
oogway <oogway543543@gmail.com>
2023-06-16 14:05:19 +0800

    0

--- rgbled.py
+++ rgbled.py
@@ -3,15 +3,15 @@ GPIO.setmode(GPIO.BCM)
 
 class RGBLed():
     def __init__(self,redpin,greenpin,bluepin):
-    GPIO.setup(red_pin,GPIO.OUT)
-    GPIO.setup(green_pin,GPIO.OUT)
-    GPIO.setup(blue_pin,GPIO.OUT)
-    self.red = (redpin,75)
-    self.green = (greenpin,75)
-    self.blue = (bluepin,75)
+        GPIO.setup(red_pin,GPIO.OUT)
+        GPIO.setup(green_pin,GPIO.OUT)
+        GPIO.setup(blue_pin,GPIO.OUT)
+        self.red = (redpin,75)
+        self.green = (greenpin,75)
+        self.blue = (bluepin,75)
 
-def close(self):
-    self.red.setup()
-    self.green.setup()
-    self.blue.setup()
-    GPIO.cleanup()
+    def close(self):
+        self.red.setup()
+        self.green.setup()
+        self.blue.setup()
+        GPIO.cleanup()
