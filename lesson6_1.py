228938b651ee892d392f61ff13c14d5b4a4d6c97
oogway <oogway543543@gmail.com>
2023-06-28 16:49:50 +0800

    0

--- lesson6_1.py
+++ lesson6_1.py
@@ -1,20 +1,34 @@
 import gpiozero as zero
 import RPi.GPIO as GPIO
 from time import sleep
+from datetime import datetime
+import requests
+
 
 
 if __name__ == "__main__":
-  print("Hello world")
-  mcp3008 = zero.MCP3008(channel=7)
-  try:
-    while True:
-       value = round(mcp3008.value*100)
-       print("light", value)
-       if value > 20:
-         print("go")
+    mcp3008_ch7 = zero.MCP3008(channel=7)
+    mcp3008_ch6 = zero.MCP3008(channel=6)
+    try:
+        while True:
+            value = round(mcp3008_ch7.value*100)
+            print("光敏電阻値: ", value)
+            if value > 20:
+                print("光線亮")
+            else:
+                print("光線暗") 
 
+            datetimeStr = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-       sleep(1)  
-  except KeyboardInterrupt:
-    GPIO.cleanup()
-    print("bye~")
+            response = requests.get(f'https://oogway543-reimagined-space-system-xj5xrjvxxq7h6gp9-8000.preview.app.github.dev/respberry?time={datetimeStr}&light={value}&temperature={mcp3008_ch6.value}')
+            
+            if response.ok:
+                print("上傳資料成功")
+                print(response.text)
+            else:
+                print(response.status_code)
+
+            sleep(5)
+    except KeyboardInterrupt:
+        GPIO.cleanup()
+        print("程序退函数")
