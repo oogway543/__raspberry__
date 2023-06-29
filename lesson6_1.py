5e00196938e4a0952deb7bc1012e35f7c1cde327
oogway <oogway543543@gmail.com>
2023-06-29 11:35:21 +0800

    0

--- lesson6_1.py
+++ lesson6_1.py
@@ -5,25 +5,24 @@ from datetime import datetime
 import requests
 
 
-
 if __name__ == "__main__":
     mcp3008_ch7 = zero.MCP3008(channel=7)
     mcp3008_ch6 = zero.MCP3008(channel=6)
     try:
         while True:
             value = round(mcp3008_ch7.value*100)
-            print("光敏電阻値: ", value)
+            print("light ", value)
             if value > 20:
-                print("光線亮")
+                print("go")
             else:
-                print("光線暗") 
-
+                print("stop") 
+            
             datetimeStr = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-            response = requests.get(f'https://oogway543-reimagined-space-system-xj5xrjvxxq7h6gp9-8000.preview.app.github.dev/respberry?time={datetimeStr}&light={value}&temperature={mcp3008_ch6.value}')
-            
+            response = requests.get(f'https://oogway543.onrender.com/raspberry?time={datetimeStr}&light={value}&temperature={mcp3008_ch6.value}')
+
             if response.ok:
-                print("上傳資料成功")
+                print("up")
                 print(response.text)
             else:
                 print(response.status_code)
